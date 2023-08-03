2aac2d129370c1b4a3e235b8bb1c8086a4dc9a88
navidkanaani <navidkaen@gmail.com>
2023-08-04 00:23:13 +0330

    Modify logs.

--- selt_test.py
+++ selt_test.py
@@ -18,7 +18,7 @@ class SeltTest:
             logger.info("Login user.")
             tn_socket.write("enable".encode("ascii") + b"\r\n")
             tn_socket.write("configure terminal".encode("ascii") + b"\r\n")
-            logger.info("Deactivating interface.")
+            logger.critical("Deactivating interface.")
             tn_socket.write(f"adsl deactivate adsl {interface_address}".encode("ascii") + b"\r\n\r\n")
             tn_socket.write(f"interface adsl {interface_address}".encode("ascii") + b"\r\n\r\n")
             logger.info("Testing interface.")
@@ -30,13 +30,13 @@ class SeltTest:
             time.sleep(10)
             try:
                 result = tn_socket.read_very_eager().decode("ascii")
-                logger.info(result)
+                logger.critical(result)
             except BrokenPipeError as error:
                 logger.error("Test failed, trying again :(")
                 tn_socket.close()
                 SeltTest.huawei_5300_selt_test(host=host, interface_address=interface_address)
             time.sleep(20)
-            logger.info("Activating interface.")
+            logger.critical("Activating interface.")
             tn_socket.write(f"adsl activate adsl {interface_address}".encode("ascii") + b"\r\n")
 
         return Utils.retrieve_line_length(result)
