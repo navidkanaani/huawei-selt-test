0a1e9619e467fa6b6966ff0e68ec0eaeb1e9d815
navidkanaani <navidkaen@gmail.com>
2023-08-10 22:39:35 +0330

    Add environment variables.

--- runtime_config.py
+++ runtime_config.py
@@ -1,4 +1,11 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+
+dotenv_path = join(dirname(__file__), ".env")
+load_dotenv(dotenv_path)
+
 class RuntimeConfig:
-    USERNAME = "Fanavasd"
-    PASSWORD = "SD@82185"
-    
+    USERNAME = os.environ.get("FANAVA_USERNAME")
+    PASSWORD = os.environ.get("FANAVA_PASSWORD")
