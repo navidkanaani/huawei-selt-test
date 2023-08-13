e1eeebfe0a8a86d2887132a2c224062a018390f5
navidkanaani <navidkaen@gmail.com>
2023-08-13 13:29:17 +0330

    Update huawei 5600 selt test API.

--- app.py
+++ app.py
@@ -18,9 +18,11 @@ def huawei_5300_selt_test(host_address: str) -> str:
 @app.route("/huawei-dslam-5600/selt-test/<string:host_address>")
 def huawei_5600_selt_test(host_address: str) -> str:
     interface_address = request.args.get("interface")
-    app.logger.critical(f"Starting test selt on host: {host_address}, interface: {interface_address}")
+    port = request.args.get("port")
+    app.logger.critical(f"Starting test selt on host: {host_address}, interface: {interface_address}, port: {port}")
     selt_test_result = SeltTest.huawei_5600_selt_test(host=escape(host_address),
-                                                      interface_address=escape(interface_address))
+                                                      interface_address=escape(interface_address),
+                                                      port=escape(port))
     return f"{selt_test_result}"
 
 if __name__ == "__main__":
