2650ea68030164579142afd3f749cae7ddaf8ad7
navidkanaani <navidkaen@gmail.com>
2023-08-13 13:31:22 +0330

    Update utils, add huawei 5600 retrieve line length utility.

--- utils.py
+++ utils.py
@@ -11,18 +11,18 @@ class Utils:
         return line_length_meters[0]
     
     @staticmethod
-    def huawei_5600_deactivate_confirm(terminal_result: str) -> bool:
+    def huawei_5600_deactivate_confirmation(terminal_result: str) -> bool:
         deactivated_status = False
         already_deactivated_message_pattern = r"(Failure:).*(deactivated)"
         deactivate_successfull_message_pattern = r"(Deactivate).*(successfully)"
-        breakpoint()
-        if re.match(already_deactivated_message_pattern, terminal_result) or re.match(deactivate_successfull_message_pattern, terminal_result):
+        if len(re.findall(already_deactivated_message_pattern, terminal_result)) or len(re.findall(deactivate_successfull_message_pattern, terminal_result)):
            deactivated_status = True 
-        return deactivated_status 
-
-result = """esf-esf-ghandi1(config-if-adsl-0/3)#deactivate 1
-  Failure: Port 1 has been deactivated"""
+        return deactivated_status
     
-    
-# match_test_result = Utils.huawei_5600_deactivate_confirm(result)
-# print(match_test_result)
+    @staticmethod
+    def huawei_5600_retrieve_line_length(result: str):
+        line_length_pattern = r"Line length.*\(\d*.feet\)"
+        line_length_meters_pattern = r"\d.+ m"
+        line_length = re.findall(line_length_pattern, result)
+        line_length_meters = re.findall(line_length_meters_pattern, line_length[0])
+        return line_length_meters[0]
